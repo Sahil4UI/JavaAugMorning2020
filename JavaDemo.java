496b47a6d87895eb3fe1cdbe088250f3448f4c60
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2020-08-10 13:01:26 +0530

    Add files via upload

---
+++ JavaDemo.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+
+public class JavaDemo {
+	void function2() {
+//		void- empty->which doesnot return anything
+		
+//	string args - >command line arguement
+//		type casting ->one type->another type
+		
+//		WrapperClass.MEthod()
+////		ClassName objectname = new ClassName();
+//		
+//		Scanner obj = new Scanner(System.in);
+//		System.out.print("Enter Number 1:\n");
+//		int a = obj.nextInt();
+//		System.out.print("Enter Number 2:\n");
+//		int b = obj.nextInt();
+//		obj.close();
+//		int c = a+b;
+//		System.out.println(c);
+//		System.out.print("Java\n");
+//		System.out.print("Java\n");
+//		int a = -5;
+		
+//		System.out.println(~a);
+//		System.out.println(5 < 10 && 5 < 12);
+//		System.out.println(15 < 10 || 5 < 12);
+		System.out.println(!(5 < 10 && 5 < 12));
+		
+	}
+
+	
+
+}
---
+++ Main.java
@@ -0,0 +1,26 @@
+
+class Main
+{
+	public static void main(String[] args) {
+		
+		java001 obj1 = new java001();
+		JavaDemo obj2 =  new JavaDemo();
+		obj2.function2();
+//		int c = obj1.Function1();
+//		System.out.print(obj1.Function1());
+//		System.out.println(c);
+		
+	}
+}
+
+
+class java001 {
+	
+	public int Function1()
+	{	int a =5;
+		int b = 10;
+		return a+b;
+	}
+
+}
+
