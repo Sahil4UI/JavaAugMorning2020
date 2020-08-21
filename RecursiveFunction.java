426f04b49ec92b84bcba4491d74dbe580b0e1300
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2020-08-21 12:49:22 +0530

    Update RecursiveFunction.java

--- RecursiveFunction.java
+++ RecursiveFunction.java
@@ -17,18 +17,50 @@ public class RecursiveFunction {
 //		System.out.println(x);
 //	}
 	
-	public static int myPower(int a, int b)
+//	public static int myPower(int a, int b)
+//	{
+//		if (b==0)
+//		{
+//			return 1;
+//		}
+//		int power = a* myPower(a,b-1);
+//		return power;
+//	}
+	
+//	public static int fibonacci(int n)
+//	{
+//		if (n<=1)
+//		{
+//			return n;
+//		}
+//		return fibonacci(n-1)+fibonacci(n-2);
+//	}
+	
+//	public static int sumDigits(int n)
+//	{
+//		if (n==0)
+//		{
+//			return 0;
+//		}
+//		return (n%10) + sumDigits(n/10);
+//	}
+	static int rev=0;
+	static int base=1;
+	public static int Reverse(int n)
 	{
-		if (b==0)
+		if(n>0)
 		{
-			return 1;
+			Reverse(n/10);
+			rev += (n%10)*base;
+			base=base*10;
 		}
-		int power = a* myPower(a,b-1);
-		return power;
+		return rev;
 	}
 	public static void main(String[] args) {
 //		myFunction(1);
-		System.out.println(myPower(5,2));
+//		System.out.println(myPower(5,2));
+//		System.out.println(fibonacci(1));
+		System.out.println(Reverse(12345));
 	}
 
 }
